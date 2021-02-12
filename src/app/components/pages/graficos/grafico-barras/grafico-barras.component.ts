4620175ee0462cfe564cdf1675ec3438776f6e1a
Diego Baes <diegobaesv@gmail.com>
2021-02-12 01:13:55 -0500

    Components create y configuraciones iniciales

---
+++ src/app/components/pages/graficos/grafico-barras/grafico-barras.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barras',
+  templateUrl: './grafico-barras.component.html',
+  styleUrls: ['./grafico-barras.component.scss']
+})
+export class GraficoBarrasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
