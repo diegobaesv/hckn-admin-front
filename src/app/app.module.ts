47e909dc0e35899bc2d3946f104dc43de7b2e927
Diego Baes <diegobaesv@gmail.com>
2021-02-12 23:14:43 -0500

    Components create y configuraciones iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { PrimengModule } from './primeng/primeng.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateMasterComponent } from './components/templates/template-master/template-master.component';
+import { GraficosComponent } from './components/pages/graficos/graficos.component';
+import { GraficoTablaComponent } from './components/pages/graficos/grafico-tabla/grafico-tabla.component';
+import { GraficoBarrasComponent } from './components/pages/graficos/grafico-barras/grafico-barras.component';
+import { MapaComponent } from './components/pages/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateMasterComponent,
+    GraficosComponent,
+    GraficoTablaComponent,
+    GraficoBarrasComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
